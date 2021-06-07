40e8a7ddddc1cd2553e9fcbf9464d45143d3d718
Mick Luypaerts <mickluypaerts@gmail.com>
2021-06-07 19:46:13 +0200

    Added the readCsv() function for ex1

--- ex1_quiz_game/main.go
+++ ex1_quiz_game/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"os"
+	"encoding/csv"
 	"fmt"
 )
 
+type QuestionAnswer struct {
+	question string
+	answer string
+}
 func main(){
+	partOne()
+}
 
+func partOne(){
+	readCsv("problems.csv")
+}
+
+func readCsv(fileName string) ([][]string, error){
+	file, err := os.Open(fileName)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	record, err := reader.Read()
+	if err != nil {
+		return [][]string{}, err
+	}
+	fmt.Println(record)
+	record, err = reader.Read()
+	fmt.Println(record[1])
+
+	return nil, err
 }
