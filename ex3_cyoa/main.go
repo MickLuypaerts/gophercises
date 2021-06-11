bd65703d71cb3c98a7f054a14c0adeed2d70de98
Mick Luypaerts <mickluypaerts@gmail.com>
2021-06-11 12:24:38 +0200

    First test for the JSON parser

--- ex3_cyoa/main.go
+++ ex3_cyoa/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+type StoryArc struct {
+	Title   string `json:"title"`
+	Story   string `json:"story"`
+	Options []Option
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	parsedJSON, err := parseJSON("gopher.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(parsedJSON)
+
+}
+
+func parseJSON(file string) (map[string]StoryArc, error) {
+
+	jsonFile, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	storyMap := make(map[string]StoryArc)
+	json.Unmarshal([]byte(byteValue), &storyMap)
+
+	return storyMap, nil
 }
