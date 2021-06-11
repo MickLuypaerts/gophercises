d22d73c364254c98105f1719fc7cd4e31f28fcce
Mick Luypaerts <mickluypaerts@gmail.com>
2021-06-11 10:48:16 +0200

    Finished the YAMLHander()

--- ex2_url_shortener/src/urlshort/handler.go
+++ ex2_url_shortener/src/urlshort/handler.go
@@ -1,8 +1,14 @@
 package urlshort
 
 import (
+	"gopkg.in/yaml.v2"
 	"net/http"
 )
+
+type YAMLStruct struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -44,7 +50,32 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
-	return func(w http.ResponseWriter, r *http.Request) {
-		fallback.ServeHTTP(w, r)
-	}, nil
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	/*
+		return func(w http.ResponseWriter, r *http.Request) {
+			fallback.ServeHTTP(w, r)
+		}, nil*/
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yml []byte) ([]YAMLStruct, error) {
+	var ymlMap []YAMLStruct
+
+	err := yaml.Unmarshal(yml, &ymlMap)
+	if err != nil {
+		return nil, err
+	}
+	return ymlMap, nil
+}
+
+func buildMap(parsedYaml []YAMLStruct) map[string]string {
+	pathMap := make(map[string]string)
+	for _, value := range parsedYaml {
+		pathMap[value.Path] = value.URL
+	}
+	return pathMap
 }
